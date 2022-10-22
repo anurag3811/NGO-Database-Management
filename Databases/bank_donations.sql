ffd0dc91640791b32d174bb7d2aa7dfec4666783
Anurag Singh <90863914+anurag3811@users.noreply.github.com>
2022-10-22 18:50:59 +0530

    clean sql

--- Databases/bank_donations.sql
+++ Databases/bank_donations.sql
@@ -1,27 +1,3 @@
--- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
---
--- Host: localhost    Database: bank
--- ------------------------------------------------------
--- Server version	8.0.30
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `donations`
---
-
-DROP TABLE IF EXISTS `donations`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `donations` (
   `pid` int NOT NULL,
   `did` int NOT NULL,
@@ -37,25 +13,16 @@ CREATE TABLE `donations` (
   CONSTRAINT `cid` FOREIGN KEY (`cid`) REFERENCES `center` (`cid`),
   CONSTRAINT `did` FOREIGN KEY (`did`) REFERENCES `donor` (`did`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `donations`
---
 
 LOCK TABLES `donations` WRITE;
-/*!40000 ALTER TABLE `donations` DISABLE KEYS */;
-INSERT INTO `donations` VALUES (1,2,8,2,'2022-05-22','shirt','L','M'),(2,3,4,1,'2027-07-22','jeans','XL','M'),(3,5,10,3,'2010-08-22','mini-skirt','S','F'),(4,4,15,4,'2022-10-17','blouse','M','F'),(5,3,5,2,'2012-12-12','saree','F','F'),(6,3,7,2,'2012-08-01','saree','L','F'),(7,3,10,3,'2012-08-01','blouse','L','F');
-/*!40000 ALTER TABLE `donations` ENABLE KEYS */;
+
+INSERT INTO `donations` VALUES 
+(1,2,8,2,'2022-05-22','shirt','L','M'),
+(2,3,4,1,'2027-07-22','jeans','XL','M'),
+(3,5,10,3,'2010-08-22','mini-skirt','S','F'),
+(4,4,15,4,'2022-10-17','blouse','M','F'),
+(5,3,5,2,'2012-12-12','saree','F','F'),
+(6,3,7,2,'2012-08-01','saree','L','F'),
+(7,3,10,3,'2012-08-01','blouse','L','F');
+
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
-
