6792794ec44634a081e4d6c1d33ef266144eef42
Anurag Singh <90863914+anurag3811@users.noreply.github.com>
2022-11-07 19:50:11 +0530

    cleaner sql

--- Databases/bank_donor.sql
+++ Databases/bank_donor.sql
@@ -4,9 +4,7 @@ CREATE TABLE `donor` (
   `dsex` varchar(10) NOT NULL,
   `dage` int NOT NULL,
   PRIMARY KEY (`did`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-LOCK TABLES `donor` WRITE;
+) 
 
 INSERT INTO `donor` VALUES 
 (1,'dhamankar','m',45),
@@ -14,5 +12,3 @@ INSERT INTO `donor` VALUES
 (3,'max','m',21),
 (4,'Sakahi','f',20),
 (5,'Amarjaa','f',89);
-
-UNLOCK TABLES;
