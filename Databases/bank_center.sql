923cc6c9de195ec8467e963c748dc6bcc908a2f5
Anurag Singh <90863914+anurag3811@users.noreply.github.com>
2022-11-07 19:47:52 +0530

    cleaner sql

--- Databases/bank_center.sql
+++ Databases/bank_center.sql
@@ -5,14 +5,10 @@ CREATE TABLE `center` (
   `chead` varchar(45) NOT NULL,
   `cphone` int NOT NULL,
   PRIMARY KEY (`cid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-LOCK TABLES `center` WRITE;
+)
 
 INSERT INTO `center` VALUES 
 (1,'Pimpri',12,'sakshi',12345),
 (2,'Katraj',8,'mahima',22345),
 (3,'Viman nagar',25,'alia',32345),
 (4,'Singhgad',15,'ananya',42345);
-
-UNLOCK TABLES;
